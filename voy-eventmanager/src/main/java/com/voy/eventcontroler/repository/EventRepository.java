6a0fc5d7e63a6819ddd62d85e8c31d3d93c9892b
caio <caio@dc3.com.br>
2015-11-12 16:48:09 -0200

    criado o metodo haveEventAccess

--- voy-eventmanager/src/main/java/com/voy/eventcontroler/repository/EventRepository.java
+++ voy-eventmanager/src/main/java/com/voy/eventcontroler/repository/EventRepository.java
@@ -17,6 +17,12 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     //@Query(nativeQuery = true, value = "select event.* from event_inviteduser join event on event_inviteduser.id_event = event.id_event where id_inviteduser = :idUser")
     Collection<Event> findByInvitedUsersAndInactive(Person person, Boolean inactive);
 
+    /**
+     * Indica se o usuario convidado possui acesso ao evento
+     * @param idEvent
+     * @param idUser
+     * @return
+     */
     @Query(nativeQuery = true, value = "select (COUNT(*) > 0) from event_inviteduser where id_event = :idEvent and id_inviteduser = :idUser")
     Boolean haveEventAccess(@Param("idEvent") Long idEvent,@Param("idUser") Long idUser);
 }
